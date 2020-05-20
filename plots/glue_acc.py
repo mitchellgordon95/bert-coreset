10c86cb6cd45b100f440f470c0ade62661dd2a8c
Mitchell Gordon <mitchell.gordon95@gmail.com>
2020-05-20 11:59:06 -0400

    Plot the average GLUE acc

--- plots/glue_acc.py
+++ plots/glue_acc.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import subprocess
 import pandas as pd
+import numpy as np
 from io import StringIO
 
 
@@ -13,7 +14,10 @@ table['Sparsity'] = pd.to_numeric(table['Sparsity'], errors='raise')
 table['acc'] = pd.to_numeric(table['acc'], errors='coerce')
 table.round(2)
 
-
+unique_sparsity = pd.unique(table['Sparsity'])
+total_uniform_acc = np.zeros(unique_sparsity.shape[0])
+total_topk_acc = np.zeros(unique_sparsity.shape[0])
+count = 0
 for task in pd.unique(table["GlueTask"]):
 
     fig, axes = plt.subplots()
@@ -28,3 +32,20 @@ for task in pd.unique(table["GlueTask"]):
     axes.set_title(f"{task} Sparsity vs Acc")
     axes.legend()
     fig.savefig(f'plots_out/sparsity_vs_acc_{task}.png')
+
+    if len(uniform["acc"]) == unique_sparsity.shape[0] and len(topk["acc"]) == unique_sparsity.shape[0]:
+        total_uniform_acc += uniform["acc"]
+        total_topk_acc += topk["acc"]
+        count += 1
+    else:
+        print(f"Skipping {task} for averages since it's not complete")
+
+fig, axes = plt.subplots()
+
+axes.set_xlabel("Sparsity")
+axes.set_ylabel("Dev Acc")
+axes.plot(unique_sparsity, total_uniform_acc / count, label="Uniform")
+axes.plot(unique_sparsity, total_topk_acc / count, label="Top-K")
+axes.set_title(f"Average GLUE Sparsity vs Acc")
+axes.legend()
+fig.savefig(f'plots_out/sparsity_vs_acc_avg.png')
